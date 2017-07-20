5d1cb45400e1c0d964fcb152c02c33db5e21ede2
sangwon0001 <sangwon0001@gmail.com>
2017-07-20 16:36:08 +0900

    c

--- myapplication2/src/main/java/kr/ac/cau/cse/myapplication/MainActivity.java
+++ myapplication2/src/main/java/kr/ac/cau/cse/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,12 +20,30 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText id,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new LoginTask().execute("abcd","password");
+        id = (EditText)findViewById(R.id.editText);
+        password = (EditText)findViewById(R.id.editText2);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(
+                        id.getText().toString().equals("")||// id값이 입력되었는지
+                        password.getText().toString().equals("")){//pw값이 입력되었는지
+
+                    Toast.makeText(MainActivity.this,"칸을 모두 채워주세요",Toast.LENGTH_SHORT).show();
+                }else {
+                    new LoginTask().execute(
+                            id.getText().toString(),
+                            password.getText().toString()
+                    );
+                }
+            }
+        });
     }
     private class LoginTask extends AsyncTask<String,Void,String> {
         @Override
@@ -68,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String dataFromServer) {
             super.onPostExecute(dataFromServer);
+            Toast.makeText(MainActivity.this,dataFromServer,Toast.LENGTH_SHORT).show();
         }
     }
 }
